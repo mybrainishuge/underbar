1881106edb8c62f98c6369f98899b3b0c4b46e41
Richard May <mybrainishuge@users.noreply.github.com>
2016-03-02 12:14:00 -0600

    Completed _.flatten

--- src/underbar.js
+++ src/underbar.js
@@ -368,6 +368,15 @@
   //
   // Hint: Use Array.isArray to check if something is an array
   _.flatten = function(nestedArray, result) {
+    result = result || [];
+    for (var i = 0; i < nestedArray.length; i++) {
+      if (Array.isArray(nestedArray[i])) {
+        result.concat(_.flatten(nestedArray[i], result));
+      } else {
+        result.push(nestedArray[i]);
+      }
+    }
+    return result;
   };
 
   // Takes an arbitrary number of arrays and produces an array that contains
