cd89c034963efaf3b9bce7897c3e942c61405ebd
Richard May <mybrainishuge@users.noreply.github.com>
2016-03-01 23:36:46 -0600

    Completed _.uniq, _.map, and _.pluck

--- src/underbar.js
+++ src/underbar.js
@@ -97,6 +97,15 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+    var unique = {};
+    _.each(array, function(value) {
+      unique[value] = value;
+    });
+    _.each(unique, function(value) {
+      result.push(value);
+    });
+    return result;
   };
 
 
@@ -105,6 +114,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(value) {
+      result.push(iterator(value));
+    });
+    return result;
   };
 
   /*
